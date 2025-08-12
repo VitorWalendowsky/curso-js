5e48671a2c8b1779fe4a8cf0532ad010df5a4551
Vitor Lopes Walendowsky <043347@PROWAY.TREINA>
2025-08-11 21:57:08 -0300

    incluso exercicio 14 necessário corrigir o mesmo

--- Exercicios/exercicios-if2.js
+++ Exercicios/exercicios-if2.js
@@ -14,41 +14,96 @@ function exercicio10() {
     let lado2 = parseFloat(prompt("Digite o lado 2:"));
     let lado3 = parseFloat(prompt("Digite o lado 3:"));
     let lado4 = parseFloat(prompt("Digite o lado 4:"));
-    if (lado1 === lado3 && lado2 === lado4 && lado1 !== lado2 )
+    if (lado1 === lado3 && lado2 === lado4 && lado1 !== lado2)
         alert("É um retangulo");
     else
         alert("Não é um retangulo");
 }
 
-function exercicio07(){
-    let num=parseInt(prompt("Digite o número para analisar"));
+function exercicio07() {
+    let num = parseInt(prompt("Digite o número para analisar"));
     let mensagem = "";
     if (num === 0) {
         mensagem += "o número é neutro\n";
-    }  else {
-            if (num > 0) {
-                mensagem += "o número é positivo\n";
-            } else {
-                mensagem += "o número é negativo\n";
+    } else {
+        if (num > 0) {
+            mensagem += "o número é positivo\n";
+        } else {
+            mensagem += "o número é negativo\n";
         } if (num > 10) {
             mensagem += "o número é maior que 10\n"
         } else if (num <= 50) {
             mensagem += "o número é menor ou igual a 50\n"
-        } if(num < -10) {
+        } if (num < -10) {
             mensagem += "o número é menor que -10\n"
-        } else if(num >= 30){
+        } else if (num >= 30) {
             mensagem += "o número é maior ou igual a 30\n"
-        } if ( num != 1) {
+        } if (num != 1) {
             mensagem += "o número é diferente de 1"
         }
     }
-    alert (mensagem)
+    alert(mensagem)
 }
 
-function exercicio14(){
+function exercicio14() {
     let n1 = parseFloat(prompt("Insira o primeiro número"));
-    let n2 = parseFloat(prompt("Insira o primeiro número"));
-    let n3 = parseFloat(prompt("Insira o primeiro número"));
-    let maior, menor, meio;
-    
+    let n2 = parseFloat(prompt("Insira o segundo número"));
+    let n3 = parseFloat(prompt("Insira o terceiro número"));
+    let menor, meio, maior;
+    if (n1 <= n2 && n1 <= n3) {
+        menor = n1;
+        if (n2 <= n3) {
+            meio = n2;
+            maior = n3;
+        } else {
+            meio = n3;
+            maior = n2;
+  }     if (n2 <= n1 && n2 <= n3) {
+            menor = n2;
+        } if (n1 <= n3) {
+            meio = n1;
+            maior = n3;
+        } else {
+            meio = n3;
+            maior = n1;
+  } if (n3 <= n1 && n3 <= n2) {
+            menor = n3;
+        } if (n2 <= n1) {
+            meio = n2;
+            maior = n1;
+        } else if(n1 <= n2) {
+            meio = n1;
+            maior = n2;
+        }
+    }
+    alert("A ordem crescente é:\n" + menor + "," + meio + "," + maior);
 }
+
+/*if (n1 <= n2 && n1 <= n3) {
+    menor = n1;
+    if (n2 <= n3) {
+        meio = n2;
+        maior = n3;
+    } else {
+        meio = n3;
+        maior = n2;
+    }
+} else if (n2 <= n1 && n2 <= n3) {
+    menor = n2;
+    if (n1 <= n3) {
+        meio = n1;
+        maior = n3;
+    } else {
+        meio = n3;
+        maior = n1;
+    }
+} else {
+    menor = n3;
+    if (n1 <= n2) {
+        meio = n1;
+        maior = n2;
+    } else {
+        meio = n2;
+        maior = n1;
+    }
+}*/
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # curso-js
+
+
+ 17. Solicite o ano de nascimento e apresente o ano escrito por extenso.
+Exemplo: 1993 deverá apresentar para o usuário “Mil novecentos e noventa e três”
+
+18. Solicite para o usuário a hora de nascimento e apresente por extenso.
+Exemplo: 19:03:29 deverá apresentar para o usuário “dezenove horas, três minutos e vinte
+e nove segundos”.
