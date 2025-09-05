181f37b4cbca46199abce1c3e8268edb0ac7ee54
Vitor Walendowsky <vitor.walendowsky@gmail.com>
2025-09-05 20:43:28 -0300

    incluso exemplo02

---
+++ assets/js/exemplo02-seletores.js
@@ -0,0 +1,7 @@
+let body = document.getElementsByTagName("body")[0];
+
+function exemploCriarh1() {
+    let h1 = document.createElement("h1");
+    h1.innerText = "Pagina de supermercado";
+    body.appendChild(h1);
+}
