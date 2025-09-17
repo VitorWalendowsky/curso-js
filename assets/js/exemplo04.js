8d0953320d1de0bac351611fc3e7418fdbd94f87
Vitor Lopes Walendowsky <043347@proway.treina>
2025-09-17 19:07:14 -0300

    inserido exemplo 04.js e feito exemplo03-crus.js

---
+++ assets/js/exemplo04.js
@@ -0,0 +1,64 @@
+// Criar uma lista de 7 carros com os atributos (marca, placa, modelo, data de emplacamento, cor).
+// let colaboradores = [
+//     {
+//         "nome": "João Silva",
+//         "data_nascimento": "1985-03-15",
+//         "cargo": "Gerente de Marketing"
+//     },
+//     {
+//         "nome": "Pedro Santos",
+//         "data_nascimento": "1982-11-10",
+//         "cargo": "Coordenador de Vendas"
+//     }
+// ]
+let carros = [
+    {
+        "marca": "Fiat",
+        "placa": "ABC-1234",
+        "modelo": "Argo",
+        "data de emplacamento": "2020-05-15" ,
+        "cor": "Vermelho"
+    },
+    {
+        "marca": "Volkswagen",
+        "placa": "XYZ-5678",
+        "modelo": "Gol",
+        "data de emplacamento": "2019-08-10",
+        "cor":"Prata"
+    },
+    {
+        "marca": "Chevrolet",
+        "placa": "DEF-4321",
+        "modelo": "Onix",
+        "data de emplacamento": "2021-01-22",
+        "cor":"Preto"
+    },
+    {
+        "marca": "Honda",
+        "placa": "GHI-8765",
+        "modelo": "Civic",
+        "data de emplacamento": "2018-03-12" ,
+        "cor":"Branco"
+    },
+    {
+        "marca": "Ford",
+        "placa": "JKL-3456",
+        "modelo": "Ka",
+        "data de emplacamento": "2017-06-09",
+        "cor": "Azul"
+    },
+    {
+        "marca": "Renault",
+        "placa": "QHP-0151",
+        "modelo": "Clio",
+        "data de emplacamento": "2022-11-05",
+        "cor": "Branco"
+    },
+    {
+        "marca": "Bmw",
+        "placa": "PQR-6543",
+        "modelo": "320i",
+        "data de emplacamento": "2023-04-04-18",
+        "cor":"Preto"
+    },
+]
